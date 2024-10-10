41980d3a9721f6c7d9c6898487013f71f386168a
romer8 <gio.busrom@gmail.com>
2024-10-10 11:21:57 -0600

    cleanup

--- nwmps/olmap.py
+++ nwmps/olmap.py
@@ -1,7 +1,7 @@
 from .utilities import (
     get_base_map_layers_dropdown,
     get_services_dropdown,
-    DATA_SERVICES
+    DATA_SERVICES,
 )
 
 from intake.source import base
@@ -10,24 +10,26 @@ import geopandas as gpd
 import requests
 import json
 
+
 class MapVisualization(base.DataSource):
     container = "python"
     version = "0.0.1"
     name = "nwmp_map"
     visualization_args = {
         "latitude": "number",
-        "longitude":"number",
+        "longitude": "number",
         "zoom": "number",
         "huc_id": "text",
         "base_map_layer": get_base_map_layers_dropdown(),
         "services": get_services_dropdown(),
-
     }
     visualization_group = "NWMP"
     visualization_label = "NWMP Map"
     visualization_type = "map"
 
-    def __init__(self,latitude, longitude, zoom, base_map_layer, services, huc_id, metadata=None):
+    def __init__(
+        self, latitude, longitude, zoom, base_map_layer, services, huc_id, metadata=None
+    ):
         # store important kwargs
         self.BASE_URL = "https://maps.water.noaa.gov/server/rest/services/nwm"
         self.center = [longitude, latitude]
@@ -38,22 +40,16 @@ class MapVisualization(base.DataSource):
         self.base_map_layer = self.get_esri_base_layer_dict(base_map_layer)
         self.service_layer = self.get_service_layer_dict()
         self.view = self.get_view_config(center=self.center, zoom=self.zoom)
-        # self.layer_huc = self.make_huc_vector_layer()
         self.map_config = self.get_map_config()
         super(MapVisualization, self).__init__(metadata=metadata)
 
     def read(self):
-        """Return a version of the xarray with all the data in memory"""
         HUC_LAYER = self.get_wbd_layer()
-        
-        # if self.layer_huc is not None:
-        #     layers = [self.base_map_layer, HUC_LAYER, self.layer_huc, self.service_layer]
-        # else:
         layers = [self.base_map_layer, HUC_LAYER, self.service_layer]
         return {
             "layers": layers,
             "view_config": self.view,
-            "map_config": self.map_config
+            "map_config": self.map_config,
         }
 
     def get_service_layers(self):
@@ -69,8 +65,10 @@ class MapVisualization(base.DataSource):
             result.append(service_dict)
         return result
 
+    # Not using this function at the moment, but it can be used to get the HUC layer in vector format
     def make_huc_vector_layer(self):
-        if len(str(self.huc_id)) < 2 or len(str(self.huc_id)) > 12: return None 
+        if len(str(self.huc_id)) < 2 or len(str(self.huc_id)) > 12:
+            return None
         huc_level = f"huc{len(str(self.huc_id))}"
         service_url = f"https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer/{int(len(str(self.huc_id))/2)}/query"
         payload = {"where": f"{huc_level} = '{self.huc_id}'", "f": "geojson"}
@@ -79,7 +77,9 @@ class MapVisualization(base.DataSource):
             return None
         gdf = gpd.read_file(rr.url)
         centroid = gdf.geometry.unary_union.centroid
-        self.view = self.get_view_config(center=[centroid.x, centroid.y], zoom=self.zoom)
+        self.view = self.get_view_config(
+            center=[centroid.x, centroid.y], zoom=self.zoom
+        )
         layer_dict = {}
         layer_dict["type"] = "VectorLayer"
         layer_dict["props"] = {
@@ -88,7 +88,6 @@ class MapVisualization(base.DataSource):
                 "props": {
                     "url": rr.url,
                     "format": {"type": "GeoJSON"},
-
                 },
             },
             "style": "Polygon",
@@ -111,7 +110,7 @@ class MapVisualization(base.DataSource):
             "name": f'{self.service.replace("_"," ").title()}',
         }
         return layer_dict
-    
+
     @staticmethod
     def get_esri_base_layer_dict(base_map_layer):
         layer_dict = {}
@@ -142,10 +141,7 @@ class MapVisualization(base.DataSource):
     def get_map_config():
         map_config = {
             "className": "ol-map",
-            "style": {
-                "width": "100%", 
-                "height": "100%"
-            }
+            "style": {"width": "100%", "height": "100%"},
         }
         return map_config
 
@@ -168,9 +164,8 @@ class MapVisualization(base.DataSource):
 
         return base_map_layers
 
-    
     def get_wbd_layer(self):
-        layer_id = int(len(str(self.huc_id))/2)
+        layer_id = int(len(str(self.huc_id)) / 2)
         huc_level = f"huc{len(str(self.huc_id))}"
         return {
             "type": "ImageLayer",
@@ -181,15 +176,13 @@ class MapVisualization(base.DataSource):
                         "url": "https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer",
                         "params": {
                             "LAYERS": f"show:{layer_id}",
-                            "layerDefs": json.dumps({ f"{layer_id}": f"{huc_level} = '{self.huc_id}'" })
-                            # "layerDefs": json.dumps({ "1": "huc2 = '06' or status='minor' or status='moderate' or status='major'" })
-                            
-                            },
+                            "layerDefs": json.dumps(
+                                {f"{layer_id}": f"{huc_level}='{self.huc_id}'"}
+                            ),
+                        },
                     },
                 },
                 "visible": False,
                 "name": "wbd Map Service",
             },
         }
-    
-
