6679bf05ccdc88a3628c53975269cdb2d78ed06c
romer8 <gio.busrom@gmail.com>
2024-10-11 09:56:42 -0600

    visible is True for wbd layer now

--- nwmps/olmap.py
+++ nwmps/olmap.py
@@ -165,7 +165,6 @@ class MapVisualization(base.DataSource):
                         },
                     },
                 },
-                "visible": False,
                 "name": "wbd Map Service",
             },
         }
