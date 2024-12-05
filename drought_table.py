4ab882016725fe820ebf69d0f5b36edfb8e9a2c1
romer8 <gio.rombus@gmail.com>
2024-12-05 01:30:24 -0700

    initial drafts for 2/3 of the drought plugin still needs the pie chart to plugin

---
+++ drought_table.py
@@ -0,0 +1,71 @@
+from intake.source import base
+import httpx
+from .utilities import get_drought_area_type_dropdown,get_drought_statistic_type,get_drought_data_type
+from datetime import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# This will be used for the TimeSeries of the NWM data
+class DroughtDataTable(base.DataSource):
+    container = "python"
+    version = "0.0.4"
+    name = "nwmp_api_drought_table_data"
+    visualization_args = {
+        "area_type": get_drought_area_type_dropdown()
+        "statistic_type": get_drought_statistic_type(),
+        "data_type": get_drought_data_type()
+    }
+    visualization_group = "NWMP"
+    visualization_label = "NWMP Drought Data Table"
+    visualization_type = "custom"
+
+    def __init__(self, area_type,statistic_type,data_type,metadata=None):
+        self.mfe_unpkg_url = "https://unpkg.com/mfe-usdm@latest/dist/remoteEntry.js"
+        self.mfe_scope = "mfe_usdm"
+        self.mfe_module = "./Table"
+        self.api_base_url = "https://droughtmonitor.unl.edu/DmData/DataTables.aspx/ReturnTabularDMAreaPercent"
+        self.area_type = area_type.split('-')[0]
+        self.area = area_type.split('-')[1]
+        self.statistic_type = statistic_type,
+        self.data_type = data_type
+        super(DroughtDataTable, self).__init__(metadata=metadata)
+
+    def read(self):
+        table_data = self.get_data_table()
+        table_columns = self.get_table_columns(table_data)
+        return {
+            "url": self.mfe_unpkg_url,
+            "scope": self.mfe_scope,
+            "module": self.mfe_module,
+            "props": {
+                "layers": layers,
+                "viewConfig": self.view,
+                "mapConfig": self.map_config,
+                "legend": self.legend,
+            },
+        }
+
+    
+    def get_table_columns(self,table_data):
+        columns = list(table_data[0].keys())
+        columns.remove('__type')
+        columns.remove('Label')
+        return columns
+
+    def get_data_table(self):
+        try:
+            client = httpx.Client(verify=False)
+            params = {'area': f"'{self.area}'", 'statstype':f"'{self.statistic_type}'" , 'diff': f"'{self.data_type}'"}
+            r = client.get(
+                url=f"{self.api_base_url}_{area_type}",
+                timeout=None,
+            )
+            data = r.json()
+            return  data.get('d', []):
+        except httpx.HTTPError as exc:
+            logger.error(f"Error while requesting {exc.request.url!r}: {exc}")
+            return []
--- nwmps/utilities.py
+++ nwmps/utilities.py
@@ -635,3 +635,211 @@ def get_centroid_huc(huc_id):
     geom = get_huc_boundary(huc_level, huc_id)
     centroid = geom.centroid
     return [centroid.x, centroid.y]
+
+
+# Drought
+def get_drought_statistic_type():
+        return [
+            {
+                "label": "Drought Statistic Type",
+                "options": [
+                    {
+                        "label": "Cumulative Percent Area",
+                        "value": "1",
+                    },
+                    {
+                        "label": "Categorical Percent Area",
+                        "value": "2",
+                    },
+                    {
+                        "label": "Cumulative Area",
+                        "value": "3",
+                    },
+                    {
+                        "label": "Categorical Area",
+                        "value": "4",
+                    },
+                ]
+            }
+        ]
+
+def get_drought_area_type_dropdown(area_type):
+    api_endpoint = 'https://droughtmonitor.unl.edu/DmData/DataTables.aspx/ReturnAOI'
+    area_types_dict = {
+        "national": "National",
+        "state": "State",
+        "climdiv": "Climate Division",
+        "dregion": "Geographic Regions",
+        "county": "County",
+        "fema": "FEMA Region",
+        "huc2": "HUC (2 digit)",
+        "huc4": "HUC (4 digit)",
+        "huc6": "HUC (6 digit)",
+        "huc8": "HUC (8 digit)",
+        "nws": "NWS Region",
+        "wfo": "NWS Weather Forecast Offices",
+        "rfc": "River Forecast Centers",
+        "tribal": "Tribal Areas",
+        "usaceds": "USACE District",
+        "usacedv": "USACE Division",
+        "chubs": "USDA Climate Hubs",
+        "rdews": "Regional Drought Early Warning System",
+        "rcc": "Regional Climate Centers",
+        "oregion": "Other Regions",
+    }
+    dropdown_items = []
+    client = httpx.Client(verify=False)
+    for key, value in area_types_dict.items():
+        dropdown_item ={
+            "label": value,
+            "options":[]
+        }
+        try:
+            response = client.get(f'{api_endpoint}?aoi="{key}"', headers={"Content-Type": "application/json"})
+            data = response.json()
+            for item in data.get('d', []):
+                dropdown_item['options'].append({
+                    "value": f'{key-item.get("Value")}',
+                    "label": item.get("Text")
+                })
+            dropdown_items.append(dropdown_item)
+        except httpx.HTTPError as exc:
+            logger.error(f"Error while requesting {exc.request.url!r}: {str(exc.__class__.__name__)}")
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+    return dropdown_items
+
+def get_drought_data_type():
+    return [
+        {
+            "label": "Drought Data Types",
+            "options": [
+                {
+                    "label": "USDM",
+                    "value": "0",
+                },
+                {
+                    "label": "7-day Change",
+                    "value": "1",
+                }
+            ]
+        }
+    ]
+
+
+def get_drought_index():
+
+    return [
+        {
+            "label": "Drought Data Index",
+            "options": [
+                {
+                    "label": "USDM",
+                    "value": "usdm",
+                },
+                {
+                    "label": "DSCI",
+                    "value": "DSCI",
+                }
+            ]
+        }
+    ]
+
+
+
+
+
+
+
+
+
+
+
+# def get_drought_area_type_dropdown():
+#     return [
+#         {
+#             "label": "Drought Area Types",
+#             "options": [
+#                 {
+#                     "label": "National",
+#                     "value": "national",
+#                 },
+#                 {
+#                     "label": "State",
+#                     "value": "state",
+#                 },
+#                 {
+#                     "label": "Climate Division",
+#                     "value": "climdiv",
+#                 },
+#                 {
+#                     "label": "Geographic Regions",
+#                     "value": "dregion",
+#                 },
+#                 {
+#                     "label": "County",
+#                     "value": "county",
+#                 },
+#                 {
+#                     "label": "FEMA Region",
+#                     "value": "fema",
+#                 },
+#                 {
+#                     "label": "HUC (2 digit)",
+#                     "value": "huc2",
+#                 },
+#                 {
+#                     "label": "HUC (4 digit)",
+#                     "value": "huc4",
+#                 },
+#                 {
+#                     "label": "HUC (6 digit)",
+#                     "value": "huc6",
+#                 },
+#                 {
+#                     "label": "HUC (8 digit)",
+#                     "value": "huc8",
+#                 },
+#                 {
+#                     "label": "NWS Region",
+#                     "value": "nws",
+#                 },
+#                 {
+#                     "label": "NWS Weather Forecast Offices",
+#                     "value": "wfo",
+#                 },
+#                 {
+#                     "label": "River Forecast Centers",
+#                     "value": "rfc",
+#                 },
+#                 {
+#                     "label": "Tribal Areas",
+#                     "value": " tribal",
+#                 },
+#                 {
+#                     "label": "USACE District",
+#                     "value": "usaceds",
+#                 },
+#                 {
+#                     "label": "USACE Division",
+#                     "value": "usacedv,
+#                 },
+#                 {
+#                     "label": "USDA Climate Hubs",
+#                     "value": "chubs",
+#                 },
+#                 {
+#                     "label": "Regional Drought Early Warning System",
+#                     "value": "rdews",
+#                 },
+#                                 {
+#                     "label": "Regional Climate Centers",
+#                     "value": "rcc",
+#                 },
+#                                 {
+#                     "label": "Other Regions",
+#                     "value": "oregion",
+#                 },  
+#             ],
+#         }
+#     ]
